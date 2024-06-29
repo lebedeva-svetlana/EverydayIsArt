d2defce78b932e8ed257d3998ebce1a133415c0f
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-06-29 20:29:00 +0300

    Change share text.

--- EverydayIsArt/src/components/Art/ShareButton/ShareButton.jsx
+++ EverydayIsArt/src/components/Art/ShareButton/ShareButton.jsx
@@ -23,7 +23,7 @@ function ShareButton({ authors, title, date, org, url }) {
         if (text.length != 0) {
             text += '\n\n';
         }
-        text += `${org}: ${url}\n\nНайдено с помощью EverydayIsArt\n\neverydayisart.ru`
+        text += `${org}: ${url}\n\nНайдено с помощью EverydayIsArt (everydayisart.ru)`
 
         navigator.clipboard.writeText(text).then(() => alert('Скопировано!'));
     }
