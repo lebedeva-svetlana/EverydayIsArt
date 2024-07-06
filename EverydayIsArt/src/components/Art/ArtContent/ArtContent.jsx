f8a3c97c69e73b04e404a144a261cd959df53ca1
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-07-06 14:39:27 +0300

    Add missing descroption parts.

--- EverydayIsArt/src/components/Art/ArtContent/ArtContent.jsx
+++ EverydayIsArt/src/components/Art/ArtContent/ArtContent.jsx
@@ -14,9 +14,23 @@ function ArtContent({ artContent, isDescNeed }) {
                     <p className="art-par art-date">{artContent.date}</p>
                 </div>
                 <div className="art-description-group">
-                    {artContent.author && Object.keys(artContent.author).map(a => <p className="art-par" key={artContent.author[a]}>{artContent.author[a]}</p>)}
+                    {artContent.author && Object.keys(artContent.author).map(p => <p className="art-par" key={artContent.author[p]}>{artContent.author[p]}</p>)}
                 </div>
-                {artContent.description && isDescNeed && <ArtDescription description={artContent.description}></ArtDescription>}
+                {isDescNeed && <>
+                    <div className="art-description-group">
+                        {artContent.placeOfOrigin && Object.keys(artContent.placeOfOrigin).map(p => <p className="art-par" key={artContent.placeOfOrigin[p]}>{artContent.placeOfOrigin[p]}</p>)}
+                    </div>
+                    <div className="art-description-group">
+                        {artContent.medium && Object.keys(artContent.medium).map(p => <p className="art-par" key={artContent.medium[p]}>{artContent.medium[p]}</p>)}
+                    </div>
+                    <div className="art-description-group">
+                        {artContent.accessNumber && <p className="art-par">{artContent.accessNumber}</p>}
+                    </div>
+                    <div className="art-description-group">
+                        {artContent.wayToGet && Object.keys(artContent.wayToGet).map(p => <p className="art-par" key={artContent.wayToGet[p]}>{artContent.wayToGet[p]}</p>)}
+                    </div>
+                    {artContent.description && isDescNeed && <ArtDescription description={artContent.description}></ArtDescription>}
+                </>}
                 <p className="art-par art-source">
                     <a href={artContent.sourceUrl} className="a-base">{artContent.sourceUrlText}</a>
                 </p>
